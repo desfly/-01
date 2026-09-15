55e90fbb9f17763cbadfefa19631601535c67cfb
desfly <155248075+desfly@users.noreply.github.com>
2026-09-15 18:46:16 +0300

    test(scan): compile M1522 SOAPHT libusb backend

--- tests/compile-libusb-backend.sh
+++ tests/compile-libusb-backend.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -eu
 pkg-config --exists libusb-1.0
-cc -std=c99 -Wall -Wextra -Werror -pedantic $(pkg-config --cflags libusb-1.0) -c src/minibox-usb/libusb_m1522.c -o /tmp/libusb_m1522.o
+CFLAGS="-std=c99 -Wall -Wextra -Werror -pedantic $(pkg-config --cflags libusb-1.0)"
+cc $CFLAGS -c src/minibox-usb/libusb_m1522.c -o /tmp/libusb_m1522.o
+cc $CFLAGS -c src/minibox-usb/scan_m1522.c -o /tmp/scan_m1522.o
