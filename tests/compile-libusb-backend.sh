7c44b47a3a7478f7a4fc56ddad2442fc7898988b
desfly <155248075+desfly@users.noreply.github.com>
2026-09-15 19:56:32 +0300

    test(scan): compile M1522 diagnostic path

--- tests/compile-libusb-backend.sh
+++ tests/compile-libusb-backend.sh
@@ -4,3 +4,6 @@ pkg-config --exists libusb-1.0
 CFLAGS="-std=c99 -Wall -Wextra -Werror -pedantic $(pkg-config --cflags libusb-1.0)"
 cc $CFLAGS -c src/minibox-usb/libusb_m1522.c -o /tmp/libusb_m1522.o
 cc $CFLAGS -c src/minibox-usb/scan_m1522.c -o /tmp/scan_m1522.o
+cc $CFLAGS -c src/minibox-scan/soapht_transport.c -o /tmp/soapht_transport.o
+cc $CFLAGS -c src/minibox-scan/soapht_m1522_io.c -o /tmp/soapht_m1522_io.o
+cc $CFLAGS -c src/minibox-usb/scan_diag.c -o /tmp/scan_diag.o
